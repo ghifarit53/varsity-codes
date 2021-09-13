b14630d348fd652667aeff89dd30baa0008fd2d1
ghifarit53 <59758342+ghifarit53@users.noreply.github.com>
2021-09-13 07:48:53 +0700

    comments update

--- ch03/04-gophers-formula.c
+++ ch03/04-gophers-formula.c
@@ -17,39 +17,47 @@ n! = n^n e^(-n)   / ( 2n + --- ) π
                 \/          3
 
 IMPLEMENTATION
-1. de
-1. create a function gopher() that requires integer n as its argument
-2. plug n into the above formula, but first create intermediary variable
+1. define PI as constant
+2. create a function gopher() that requires integer n as its argument
+3. plug n into the above formula, but first create intermediary variable
    to make debugging easier
-3. return the result
-4. create a main function and prompts the user to enter n
-5. call gopher() with n as the arguments
-6. print the result as requested
+4. return the result
+5. create a main function and prompts the user to enter n
+6. call gopher() with n as the arguments
+7. print the result as requested
 
 */
 
 #include <math.h>
 #include <stdio.h>
 
+// 1. define PI as constant
 #define PI 3.14159265
 
+// 2. create a function gopher() that requires integer n as its argument
 double gopher(int n) {
+  // 3. plug n into the above formula, but first create intermediary variable
+  //    to make debugging easier
   double inside_sqrt = 2 * (double)n + (1 / 3);
   double inside_sqrt_pi = inside_sqrt * PI;
 
   double n_factorial = pow(n, n) * exp(-1 * n) * sqrt(inside_sqrt_pi);
 
+  // 4. return the result
   return n_factorial;
 }
 
+// 5. create a main function and prompts the user to enter n
 int main() {
   int n;
 
   printf("Enter n > ");
   scanf("%d", &n);
 
+  // 6. call gopher() with n as the arguments
   double n_factorial = gopher(n);
 
+  // 7. print the result as requested
   printf("%d! equals approximately %.5lf\n", n, n_factorial);
 
   return 0;
