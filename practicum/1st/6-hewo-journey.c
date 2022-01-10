1e6984fc9570f5904303659eda2f4d969e92625c
ghifarit53 <59758342+ghifarit53@users.noreply.github.com>
2022-01-10 22:03:55 +0700

    change how prime numbers are handled

--- practicum/1st/6-hewo-journey.c
+++ practicum/1st/6-hewo-journey.c
@@ -10,17 +10,21 @@ int main() {
   case 3:
   case 5:
   case 7:
+  case 11:
+  case 13:
+  case 17:
+  case 19:
+  case 23:
+  case 29:
+  case 31:
+  case 37:
+  case 41:
+  case 47:
     printf("IT IS A PRIME");
-    break;
+    break
   default:
-    if ((n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0 && n % 11 != 0 &&
-         n % 13 != 0 && n % 17 != 0 && n % 19 != 0 && n % 23 != 0 &&
-         n % 29 != 0 && n % 31 != 0 && n % 37 != 0 && n % 41 != 0) &&
-        (n != 4 && n != 6 && n != 8 && n != 9)) {
-      printf("IT IS A PRIME");
-    } else {
-      printf("IT IS NOT A PRIME");
-    }
+    printf("IT IS NOT A PRIME");
+    break;
   }
 
   return 0;
